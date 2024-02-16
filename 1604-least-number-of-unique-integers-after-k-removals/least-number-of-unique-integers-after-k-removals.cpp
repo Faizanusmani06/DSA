69ba19819d9dfa538b3fd427e1e4af5260d1b205
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-16 10:13:04 +0530

    Time: 125 ms (72.19%) | Memory: 64.7 MB (74.39%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         unordered_map<int, int> mp;
-        for(int i: arr) {
+        for (auto i : arr) {
             mp[i]++;
         }
-        map<int, vector<int>> new_mp;
-        auto it = mp.begin();
-        while(it != mp.end()) {
-            new_mp[it->second].push_back(it->first);
-            it++;
+        int n = mp.size();
+        vector<int> temp(n);
+        int j = 0;
+        for (auto i : mp) {
+            temp[j] = i.second;
+            j++;
         }
-        int ans = 0;
-        auto it1 = new_mp.begin();
-        while(it1 != new_mp.end()) {
-            if(k) {
-                int total = it1->first*(it1->second.size());
-                if(k >= total) {
-                    k -= total;
-                }else {
-                    ans += (it1->second.size() - k/it1->first);
-                    k = 0;
-                }
-            }else {
-                ans += (it1->second.size());
-            }
-            it1++;
-        }
+        sort(temp.begin(), temp.end());
 
+        j = 0;
+        int ans = temp.size();
+        while (k > 0) {
+            while (temp[j] > 0 && k > 0) {
+                temp[j]--;
+                k--;
+            }
+            if (temp[j] == 0) {
+                j++;
+                ans--;
+            }
+        }
         return ans;
     }
 };
