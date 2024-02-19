3ec5329822a5a9f40f0576fdd75fbd0a6198191f
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-20 01:42:23 +0530

    Time: 4 ms (80.66%) | Memory: 8.7 MB (34.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,25 +3,24 @@ public:
     
 
     bool isPalindrome(string s) {
-       string str = "";
-       for(char ch : s) {
-           int ascii = int(ch);
-           if((ascii >= 97 && ascii <= 122) || (ascii >= 48 && ascii <= 57)) {
-               str.push_back(ch);
-           }else if((ascii >= 65 && ascii <= 90)) {
-               str.push_back(ch+32);
-           }
-       }
+        int i = 0;
+        int j = s.size() - 1;
+        while(i < j) {
+            if(!isalnum(s[i])) {
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])) {
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            else {
+                i++;
+                j--;
+            }
+        }
 
-       int i = 0, j = str.size() - 1;
-       while(i < j) {
-           if(str[i] != str[j]) return false;
-           else {
-               i++;
-               j--;
-           }
-       }
-
-       return true;
+        return true;
     }
 };
