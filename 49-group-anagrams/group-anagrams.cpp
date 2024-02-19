a5a1c1a7bd4b34ffe223fcb04b2433bfa529f57b
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-20 00:13:24 +0530

    Time: 20 ms (91.44%) | Memory: 22.4 MB (77.07%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        int n = strs.size();
+
+        unordered_map<string, int> mp;
+        for(int i = 0; i < n; i++) {
+            string str = strs[i];
+            sort(str.begin(), str.end());
+
+            if(!mp.contains(str)) {
+                ans.push_back({strs[i]});
+                mp[str] = ans.size() - 1;
+            }else {
+                ans[mp[str]].push_back(strs[i]);
+            }
+        }
+
+        return ans;
+    }
+};
