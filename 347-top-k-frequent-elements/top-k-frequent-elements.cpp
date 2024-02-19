6fce8b915107b9b3852f43d988f569e71b220c31
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-20 00:38:52 +0530

    Time: 16 ms (34.42%) | Memory: 17.4 MB (12.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,13 +5,26 @@ public:
         vector<int> ans;
         for(int i : nums) mp1[i]++;
 
-        vector<pair<int, int>> arr;
-        for(auto a: mp1) {
-            arr.push_back({a.second, a.first});
-        }
-        sort(arr.rbegin(), arr.rend());
+        // vector<pair<int, int>> arr;
+        // for(auto a: mp1) {
+        //     arr.push_back({a.second, a.first});
+        // }
+        // sort(arr.rbegin(), arr.rend());
 
-        for(int i = 0; i < k; i++) ans.push_back(arr[i].second);
+        // for(int i = 0; i < k; i++) ans.push_back(arr[i].second);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        for(auto i: mp1) {
+            minHeap.push({i.second, i.first});
+            if(minHeap.size() > k) minHeap.pop();
+        }
+
+        while(k--) {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
+        return ans;
        
         return ans;
     }
