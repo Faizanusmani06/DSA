f855825eb3d9d3a83821a7242d31f061e0cfcf7f
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-20 08:53:59 +0530

    Time: 20 ms (26.34%) | Memory: 20.4 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,9 +5,6 @@ public:
         int sum = n*(n+1)/2;
         
         return sum - accumulate(nums.begin(), nums.end(), 0);
-        // for(int i = 0; i < n; i++){
-        //     sum -= nums[i];
-        // }
-        // return sum;
+        
     }
 };
