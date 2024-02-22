7442da4f502c471eff6c0d6735406689d5108e2d
MOH FAIZAN <faizanusmani06@gmail.com>
2024-02-22 11:04:37 +0530

    Time: 9 ms (42.85%) | Memory: 8.8 MB (27.17%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,30 +1,14 @@
 class Solution {
-private:
-    int find_msb(int value) {
-        int ans = 0;
-        while(value) {
-            value >>= 1;
-            ans++;
-        }
-        return ans-1;
-    }
 public:
     int rangeBitwiseAnd(int left, int right) {
         
-        int ans = 0;
-        while(left && right) {
-            int msb_left = find_msb(left);
-            int msb_right = find_msb(right);
-
-            if(msb_left != msb_right) break;
-
-            int msb_value = 1 << msb_left;
-
-            ans += msb_value;
-            left -= msb_value;
-            right -= msb_value;
+        int cnt = 0;
+        while(left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
         }
 
-        return ans;
+        return left << cnt;
     }
 };
