dad78e15154a00d1ac0b3b6173f5b95632a99721
MOH FAIZAN <faizanusmani06@gmail.com>
2024-03-01 08:46:18 +0530

    Time: 5 ms (36.76%) | Memory: 10.9 MB (8.46%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        string ans = "";
-        int n = s.size();
-        for(char ch : s) {
-            if(ch == '1') {
-                ans = ch + ans; 
-            }else ans += ch;
-        }
-
-        return ans.substr(1, n)+'1';
+        sort(s.rbegin(), s.rend());
+        return s.substr(1, s.size())+'1';
     }
 };
