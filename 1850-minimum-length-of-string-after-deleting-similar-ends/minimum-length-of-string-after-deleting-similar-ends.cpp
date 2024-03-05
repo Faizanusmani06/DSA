23ab9921817d409445822b92269e48f7b247f4b1
MOH FAIZAN <faizanusmani06@gmail.com>
2024-03-05 15:17:24 +0530

    Time: 31 ms (18.28%) | Memory: 14 MB (13.45%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n = s.size();
+        int i = 0, j = n-1;
+        
+        if(s[i] != s[j]) return j-i+1;
+        while(i <= j) {
+
+            if(i==j) return 1;
+            char c = s[i];
+
+            while(s[i] == c && i < n) i++;
+            while(s[j] == c && j > 0) j--;
+
+            if(s[i] != s[j] && i < n && j > 0) return j-i+1;
+        }
+
+        return 0;
+    }
+};
