8e5a110b14925a40d582ce10844a25bb7456d80f
MOH FAIZAN <faizanusmani06@gmail.com>
2024-07-18 17:41:40 +0530

    Time: 5 ms (25.01%) | Memory: 12 MB (18.96%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,15 +12,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-    if(p==nullptr && q == nullptr) return 1;
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
 
-    if((p==nullptr || q == nullptr) || (p->val != q->val)) return 0;
-
-
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-
-    
-    
-}
-
+        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
 };
