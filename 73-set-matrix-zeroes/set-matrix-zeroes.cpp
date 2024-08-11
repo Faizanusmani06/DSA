70f49c365b7e8e6456b368a126a97c8971fac670
MOH FAIZAN <faizanusmani06@gmail.com>
2024-08-12 01:55:43 +0530

    Time: 12 ms (40.03%) | Memory: 16.9 MB (20.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        bool col0 = false;
 
-        int col0 = 1;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(!matrix[i][j]) {
-                    // marks 0th column
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix[i].size(); j++) {
+                if(matrix[i][j] == 0) {
                     matrix[i][0] = 0;
-
-                    // marks 0th row
-                    if(j!=0) {
-                        matrix[0][j] = 0;
-                    }else col0 = 0;
+                    if(j!=0) matrix[0][j] = 0;
+                    else col0 = true;
                 }
             }
         }
 
-        // marks matrix from (1, 1) to (m-1, n-1)
+        for(int i = 1; i < matrix.size(); i++) {
+            for(int j = 1; j < matrix[i].size(); j++) {
 
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(!matrix[i][0] || !matrix[0][j]) {
-                    matrix[i][j] = 0;
-                }
+                if(matrix[i][j] && (matrix[i][0] == 0 || matrix[0][j] == 0)) matrix[i][j] = 0;
             }
         }
 
-        if(!matrix[0][0]) {
-            for(int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
+        if(matrix[0][0] == 0) {
+            for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
         }
-
-        if(!col0) {
-            for(int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
+        
+        if(col0) {
+            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
         }
+        
     }
 };
