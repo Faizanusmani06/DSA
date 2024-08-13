da447058aaf7224c11497148f45965783e671fdf
MOH FAIZAN <faizanusmani06@gmail.com>
2024-08-14 01:35:48 +0530

    Time: 55 ms (86.87%) | Memory: 27.5 MB (68.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+       vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
         int n = nums.size();
-        if(n < 3 || nums[0] > 0) return {};
 
-        for(int i = 0; i < n; i++) {
+        if(n<3 || nums[0] > 0) return {};
+
+    
+        for(int i = 0; i < n-2; i++) {
             if(nums[i] > 0) break;
+            int j = i+1;
+            int k = n-1;
 
             if(i>0 && nums[i] == nums[i-1]) continue;
+            while(j < k) {
+                int curSum = nums[i] + nums[j] + nums[k];
+                if(curSum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    int last_j = nums[j];
+                    int last_k = nums[k];
 
-
-            int l = i+1, r = n-1;
-            int sum = 0;
-            while(l < r) {
-                sum = nums[i] + nums[l] + nums[r];
-
-                if(sum > 0) r--;
-                else if(sum < 0) l++;
-                else {
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    int last_l = nums[l], last_r = nums[r];
-
-                    while(l < r && nums[l] == last_l) l++;
-                    while(l < r && nums[r] == last_r) r--;
+                    while(j < k && last_j == nums[j]) j++;
+                    while(j < k && last_k == nums[k]) k--;
                 }
+                else if(curSum > 0) k--;
+                else j++;
             }
-        }
 
+        }
+        
         return ans;
     }
 };
