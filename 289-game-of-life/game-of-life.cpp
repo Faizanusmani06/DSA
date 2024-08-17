021a9a276f9dc6650a27b4683d27034980bb80c6
MOH FAIZAN <faizanusmani06@gmail.com>
2024-08-18 00:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.15%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         vector<vector<int>> dummy = board;
-        
-
+        // 0->1 = -1
+        // 1->0 = -2;
+        // 1->1 = -3;
         int m = board.size();
         int n = board[0].size();
         int r[] = {-1, -1, -1, 0, 0, 1, 1, 1};
@@ -17,13 +18,31 @@ public:
                     int cols = j + c[k];
         
         
-                    if((rows >= 0 && rows < m) && (cols >= 0 && cols < n) && dummy[rows][cols] == 1 )  cnt++;
+                    if((rows >= 0 && rows < m) && (cols >= 0 && cols < n)  ) {
+                        if(board[rows][cols] == 1 || board[rows][cols]  == -2 || board[rows][cols]  == -3) cnt++;
+                    }
 
                 }
-                if(dummy[i][j] && cnt < 2) board[i][j] = 0;
-                else if(dummy[i][j] && (cnt == 2 || cnt == 3)) board[i][j] = 1;
-                else if(dummy[i][j] && cnt > 3) board[i][j] = 0;
-                else if(!dummy[i][j] && cnt == 3) board[i][j] = 1;
+                if(board[i][j] && cnt < 2) {
+                    board[i][j] = -2;
+                }
+                else if(board[i][j] && (cnt == 2 || cnt == 3)) {
+                    board[i][j] = -3;
+                }
+                else if(board[i][j] && cnt > 3) {
+                    board[i][j] = -2;
+                }
+                else if(!board[i][j] && cnt == 3) {
+                    board[i][j] = -1;
+                }
+
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == -1 || board[i][j] == -3) board[i][j] = 1;
+                if(board[i][j] == -2) board[i][j] = 0;
             }
         }
     }
