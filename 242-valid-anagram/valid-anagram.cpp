f7710bb9ebdd0387ad9bf0ab5d20a59797566f16
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-07 22:01:05 +0530

    Time: 18 ms (6.22%) | Memory: 10 MB (30.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        if(n1!=n2) return false;
+        map<char, int> mp;
 
-        unordered_map<char, int> mp;
-
-        for(int i = 0; i < n1; i++) {
-            mp[s[i]]++;
-        }
-        for(int i = 0; i < n1; i++) {
-            mp[t[i]]--;
-            if(!mp[t[i]]) mp.erase(t[i]);
+        for(auto ch : s) {
+            mp[ch]++;
         }
 
-        if(!mp.size()) return true;
-        return false;
+        for(auto ch: t) {
+            mp[ch]--;
+            if(mp[ch] == 0) mp.erase(ch);
+        }
+
+        if(mp.size() == 0) return true;
+        else return false;
     }
 };
