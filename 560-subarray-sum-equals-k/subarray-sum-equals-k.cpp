d5715e7d3cf3d0f9a2fad6f17eab7524b8074c19
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-14 19:21:39 +0530

    Time: 93 ms (45.92%) | Memory: 47.8 MB (39.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,16 +3,15 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int cnt = 0, sum = 0;
         map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++) {
 
-            sum += nums[i];
-            
+        for(int i : nums) {
+            sum += i;
+
             if(sum == k) cnt++;
 
-
             if(mp.find(sum - k) != mp.end()) cnt += mp[sum - k];
-            mp[sum]++;
 
+            mp[sum]++;
         }
 
         return cnt;
