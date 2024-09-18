32d7af905a26b3eb1388aa99dad7116a4761e6ea
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-18 23:47:02 +0530

    Time: 118 ms (32.18%) | Memory: 69.3 MB (80.08%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -9,29 +9,31 @@ public:
         bool f = false;
         int low = minVal, high = maxVal;
 
-
-        while(low <= high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
 
             int cnt = 0, bq = 0;
-            for(int j = 0; j < n; j++) {
-                
-                if(bloomDay[j] <= mid) cnt++;
-                else cnt = 0;
+            for (int j = 0; j < n; j++) {
 
-                if(cnt == k) {
+                if (bloomDay[j] <= mid)
+                    cnt++;
+                else
+                    cnt = 0;
+
+                if (cnt == k) {
                     bq++;
                     cnt = 0;
                 }
             }
-            
-            if(bq >= m) {
+
+            if (bq >= m) {
                 f = true;
                 high = mid - 1;
-            }
-            else low = mid + 1;
+            } else
+                low = mid + 1;
         }
-        if(!f) return -1;
+        if (!f)
+            return -1;
         return low;
     }
 };
