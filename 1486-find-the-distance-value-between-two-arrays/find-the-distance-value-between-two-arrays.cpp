ea87c802cf6571bd0325baf9d083ad2fcb6a6614
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-21 01:17:39 +0530

    Time: 11 ms (33.41%) | Memory: 16.1 MB (17.79%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        // sort(arr2.begin(), arr2.end());
+        sort(arr2.begin(), arr2.end());
         int cnt = 0;
-        for(int i = 0; i < arr1.size(); i++) {
-            for(int j = 0; j < arr2.size(); j++) {
-                if(abs(arr1[i] - arr2[j]) <= d) {
-                    cnt++;
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+        for(int i = 0; i < n1; i++) {
+            int ele = arr1[i];
+            bool eq = false;
+            int low = 0, high =  n2 - 1;
+
+            while(low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if(arr2[mid] == ele) {
+                    eq = true;
                     break;
-                }
+                }else if (arr2[mid] > ele) high = mid - 1;
+                else low = mid + 1;
             }
+            if(eq) continue;
+            cout << low << " " << high << endl;
+            if(high < 0 && arr2[low] - ele > d) cnt++;
+            else if(low >= n2 && ele - arr2[high]> d) cnt++;
+            else if(low < n2 && high >= 0 && ( (arr2[low] - ele) > d ) && ((ele - arr2[high]) > d)) cnt++; 
+            
         }
 
-        return arr1.size() - cnt;
+        return cnt;
     }
 };
