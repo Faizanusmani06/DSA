dde00ba238fa1fb7adad5057a3ae4d153823c1f8
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-26 02:19:54 +0530

    Time: 195 ms (5.09%) | Memory: 8.6 MB (12.28%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    double solve(double n) {
+        cout << n << endl;
+        if(n <= 3) {
+            if(n == 3 ) return 1;
+            else return 0;
+        }
+        return solve(n/3);
+    }
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        if(n==1) return true;
+        if(solve(double(n))) return true;
+        return false;
+    }
+};
