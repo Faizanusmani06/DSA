c2ae2a51de9a2c5fc19549c3ca2d7b97be14d8a0
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-26 20:51:03 +0530

    Time: 156 ms (24.36%) | Memory: 61.2 MB (61.32%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool isPalindrome(int i, int j, string s) {
+        while(j > i) {
+            if(s[i++] != s[j--]) return false;
+        }
+        return true;
+    }
+    void solve(int ind, vector<string> &ds, vector<vector<string>> &ans, string s) {
+
+        if(ind == s.size()) {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = ind; i < s.size(); i++) {
+            if(isPalindrome(ind, i, s)) {
+                cout << i << endl;;
+                ds.push_back(s.substr(ind, i+1-ind));
+                solve(i+1, ds, ans, s);
+                ds.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> ds;
+
+        solve(0, ds, ans, s);
+
+        return ans;
+    }
+};
