b6d60895b7a9400e8ec6bb02c3778abe57c57324
MOH FAIZAN <faizanusmani06@gmail.com>
2024-09-27 01:30:08 +0530

    Time: 1038 ms (20.97%) | Memory: 12.1 MB (17.98%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,17 +1,19 @@
 class Solution {
-    int n, m, s;
-    vector<vector<int>> visited;
-    bool dfs(int i, int j, int k, vector<vector<char>>& board, string word) {
+ 
+    
+    bool dfs(int i, int j, int k, vector<vector<char>>& board, string word, vector<vector<int>> &visited, int s, int m, int n) {
         if(i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || board[i][j] != word[k]) return 0;
 
         if(k == s-1) return true;
 
         visited[i][j] = 1;
 
-        bool a1 = dfs(i+1, j, k+1, board, word);
-        bool a2 = dfs(i, j+1, k+1, board, word);
-        bool a3 = dfs(i-1, j, k+1, board, word);
-        bool a4 = dfs(i, j-1, k+1, board, word);
+
+
+        bool a1 = dfs(i+1, j, k+1, board, word, visited, s, m, n);
+        bool a2 = dfs(i, j+1, k+1, board, word, visited, s, m, n);
+        bool a3 = dfs(i-1, j, k+1, board, word, visited, s, m, n);
+        bool a4 = dfs(i, j-1, k+1, board, word, visited, s, m, n);
 
         visited[i][j] = 0;
 
@@ -19,15 +21,15 @@ class Solution {
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        visited.assign(m, vector<int>(n, 0));
-        s = word.size();
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> visited(m, vector<int> (n, 0));
+        int s = word.size();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(board[i][j] == word[0]) {
-                    if(dfs(i, j, 0, board, word)) return 1;
+                    if(dfs(i, j, 0, board, word, visited, s, m, n)) return 1;
                 }
             }
         }
