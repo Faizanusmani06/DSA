d910bf7f21d0a222f59c4adf71a38def51d82fe1
MOH FAIZAN <faizanusmani06@gmail.com>
2024-10-05 04:39:10 +0530

    Time: 279 ms (30.63%) | Memory: 122 MB (80.20%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,46 +1,31 @@
 class MedianFinder {
-    priority_queue<int, vector<int>, greater<int>> pqMin;
-    priority_queue<int> pqMax;
 public:
-    MedianFinder() {
+    priority_queue<int>left; // max heap
+    priority_queue<int,vector<int>,greater<int>>right; // min heap
+    MedianFinder() { // default constructor
         
     }
     
-    void addNum(int num) {
-        int m = pqMax.size();
-        int n = pqMin.size();
-        if((m-n) < 1) {
-            if(!pqMin.size() || pqMin.top() >= num)
-                pqMax.push(num);
-            else {
-                int x = pqMin.top(); pqMin.pop();
-                pqMin.push(num);
-                pqMax.push(x);
-            }
-        }else {
-            if(pqMax.top() <= num) {
-                pqMin.push(num);
-            }else {
-                int x = pqMax.top(); pqMax.pop();
-                pqMax.push(num);
-                pqMin.push(x);
-
-            }
+    void addNum(int num) { // O(logn)
+        if(left.size() == 0 || num < left.top()) left.push(num);
+        else right.push(num);
+        // if left has 2 more element than right
+        if(left.size() > right.size()+1){
+            right.push(left.top());
+            left.pop();
+        }
+        // if right has 2 more element than left
+        if(right.size() > left.size()+1){
+            left.push(right.top());
+            right.pop();
         }
     }
     
-    double findMedian() {
-        int m = pqMax.size();
-        int n = pqMin.size();
-
-        if((m+n) % 2 != 0) return pqMax.top();
-        else return (pqMax.top() + pqMin.top()) * 1.0 / 2;
+    double findMedian() { // O(1)
+        if(left.size() == right.size()) return (left.top()+right.top())/2.0;
+        else {
+            if(left.size() > right.size()) return left.top();
+            else return right.top();
+        }
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
