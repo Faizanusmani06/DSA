850bf4f01e2deed99c4b1c980ad25a40b4504c3d
MOH FAIZAN <faizanusmani06@gmail.com>
2024-10-05 02:06:29 +0530

    Time: 6 ms (95.34%) | Memory: 17.5 MB (56.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp;
+        for(int i : nums) mp[i]++;
+        priority_queue<pair<int, int>> pq;
+
+        for(auto it: mp) {
+            pq.push({it.second, it.first});
+        }
+
         vector<int> ans;
-        for(int i : nums) mp1[i]++;
-
-        // vector<pair<int, int>> arr;
-        // for(auto a: mp1) {
-        //     arr.push_back({a.second, a.first});
-        // }
-        // sort(arr.rbegin(), arr.rend());
-
-        // for(int i = 0; i < k; i++) ans.push_back(arr[i].second);
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        for(auto i: mp1) {
-            minHeap.push({i.second, i.first});
-            if(minHeap.size() > k) minHeap.pop();
+        while(k) {
+            int x = pq.top().second;
+            pq.pop();
+            ans.push_back(x);
+            k--;
         }
 
-        while(k--) {
-            ans.push_back(minHeap.top().second);
-            minHeap.pop();
-        }
-
-        return ans;
-       
         return ans;
     }
 };
