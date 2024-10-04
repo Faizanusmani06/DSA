6e21e537ff8820705bfd43326b383742d2906e3b
MOH FAIZAN <faizanusmani06@gmail.com>
2024-10-05 05:04:33 +0530

    Time: 5 ms (39.88%) | Memory: 15.8 MB (5.38%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if(nums.size() < 2) return -1;
         sort(nums.rbegin(), nums.rend());
         return (nums[0] - 1) * (nums[1] - 1);
     }
