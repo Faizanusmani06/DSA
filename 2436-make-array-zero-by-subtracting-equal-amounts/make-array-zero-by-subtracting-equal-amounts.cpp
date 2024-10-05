963457f34e52d7ebd50cc1bf83598a5f9c44aad9
MOH FAIZAN <faizanusmani06@gmail.com>
2024-10-05 15:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (18.62%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        return st.size() - st.count(0);
+    }
+};
