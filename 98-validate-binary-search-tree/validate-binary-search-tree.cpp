9abb56aeb22e850f689343b96e5a6641670b1ed8
MOH FAIZAN <faizanusmani06@gmail.com>
2024-11-11 22:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (21.33%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,21 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-private:
-    bool isValid(TreeNode* root, long long minVal, long long maxVal) {
-        if(!root) return true;
-        if(root->val >= maxVal || root->val <= minVal) return false;
-        return isValid(root->left, minVal, root->val) && isValid(root->right, root->val, maxVal);
-    }
-public:
-    bool isValidBST(TreeNode* root) {
-        long long int x = -1e12, y = 1e12;
+    bool solve(TreeNode* node, long min, long max) {
+        if (!node)
+            return true;
 
-        return isValid(root, x, y);
-        
+        if (!(node->val > min && node->val < max))
+            return false;
+
+        return solve(node->left, min, node->val) &&
+               solve(node->right, node->val, max);
     }
+
+public:
+    bool isValidBST(TreeNode* root) { return solve(root, LONG_MIN, LONG_MAX); }
 };
