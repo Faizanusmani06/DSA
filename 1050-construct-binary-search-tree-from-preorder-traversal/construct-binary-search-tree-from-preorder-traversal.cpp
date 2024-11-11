afae86ab0d31ef75c05c756e69211e0b0f04e847
MOH FAIZAN <faizanusmani06@gmail.com>
2024-11-12 01:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (60.08%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,23 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-private:
-    TreeNode* build (vector<int> &A, int &i, int bound) {
-        if(i == A.size() || A[i] > bound) return nullptr;
+    TreeNode* solve(vector<int>& arr, int &i, int bound) {
+        if (i == arr.size() || arr[i] > bound)
+            return nullptr;
 
-        TreeNode* root = new TreeNode(A[i++]);
-        root->left = build(A, i, root->val);
-        root->right = build(A, i, bound);
+        TreeNode* root = new TreeNode(arr[i++]);
+        root->left = solve(arr, i, root->val);
+        root->right = solve(arr, i, bound);
 
         return root;
     }
+
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         int i = 0;
-        return build(preorder, i, INT_MAX);
+        return solve(preorder, i, INT_MAX);
     }
 };
