5f803de2b58e096eb3f9488089e1cb27650f693e
MOH FAIZAN <faizanusmani06@gmail.com>
2025-01-03 23:50:15 +0530

    Time: 4 ms (24.13%) | Memory: 26.6 MB (19.75%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         vector<int> ans;
         for(int i : nums) {
             mp[i]++;
