125a6b90b65e98b71793beefc2cf0c0dcbfae71d
MOH FAIZAN <faizanusmani06@gmail.com>
2025-01-04 00:19:48 +0530

    Time: 2 ms (43.01%) | Memory: 25.2 MB (73.13%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> ans;
-        for(int i : nums) {
-            mp[i]++;
-            if(mp[i] == 2) ans.push_back(i);
+        int xorSum = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) xorSum ^= nums[i];
+
+        for(int i = 0; i < n-2; i++) xorSum ^= i;
+
+        int rightMostSetBit = xorSum & ~(xorSum - 1);
+
+
+        int first = 0;
+        int second = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] & rightMostSetBit) {
+                first ^= nums[i];
+            }else second ^= nums[i];
         }
-        return ans;
+
+        for(int i = 0; i < n-2; i++) {
+            if(i & rightMostSetBit) {
+                first ^= i;
+            }else second ^= i;
+        }
+
+        return {first, second};
+
     }
 };
