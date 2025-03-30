ebc6096f4cac0a81936bab061f8425fbf8621ef7
MOH FAIZAN <faizanusmani06@gmail.com>
2025-03-30 14:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (18.69%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
 
         int sum = 0;
-        int maxSum = 0;
+        int maxSum = INT_MIN;
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
+            if (sum < 0)
+                sum = 0;
             sum += nums[i];
-
-            if(sum < 0) sum = 0;
 
             maxSum = max(maxSum, sum);
         }
-        if(maxSum == 0) return *max_element(nums.begin(), nums.end());
+
         return maxSum;
     }
 };
