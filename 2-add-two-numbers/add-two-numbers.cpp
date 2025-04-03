0ca11a28d9f2a97b8ff2a1e37f66fb8eeb50c304
MOH FAIZAN <faizanusmani06@gmail.com>
2025-04-03 12:15:01 +0530

    Time: 2 ms (40.05%) | Memory: 77 MB (73.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,37 +1,44 @@
-// **
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode() : val(0), next(nullptr) {}
-//  *     ListNode(int x) : val(x), next(nullptr) {}
-//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-//  * };
-//  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail  = dummyHead;
-        int carry = 0;
-        while(l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
 
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = 0;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            sum += carry;
+
             carry = sum / 10;
 
-            ListNode * newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
+            ListNode* newNode = new ListNode(sum % 10);
 
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+            temp->next = newNode;
+            temp = temp->next;
         }
 
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+        return dummyNode->next;
     }
 };
